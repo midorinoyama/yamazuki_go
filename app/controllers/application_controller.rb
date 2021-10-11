39bc115ce2e8f605d3aa8e2ff144a6cb1313ec53
midorinoyama <fujiaqa@gmail.com>
2021-10-11 05:43:56 +0000

    Add nickname-column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #devise認証機能が使われる時はその前に、 configure_permitted_parametersが実行される
+  protected
+  
+  def configure_permitted_parameters
+    devise_patameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    #会員登録nicknameのデータ許可
+  end  
 end
