da5a00ef7218bb7e29df1420d7fac08f73e4f1f0
midorinoyama <fujiaqa@gmail.com>
2021-10-13 07:54:36 +0000

    Add 投稿機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.ner(post_params)
+    @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to post_path
+    redirect_to posts_path
   end
 
   def index
@@ -15,15 +15,23 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
