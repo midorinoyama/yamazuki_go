85fd6b1f509fc4c7fece097a83860b3e31380cac
midorinoyama <fujiaqa@gmail.com>
2021-10-14 06:54:35 +0000

    Adduserカラム

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   #deivseを使用するURLにusersを含む
   root to: 'homes#top'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_051441) do
+ActiveRecord::Schema.define(version: 2021_10_14_064328) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 2021_10_14_051441) do
     t.string "nickname", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
+    t.text "introduction"
+    t.integer "gender", default: 0
+    t.integer "age", default: 0
+    t.integer "prefecture", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
