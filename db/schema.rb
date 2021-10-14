e1c34ed6493606f458bb10e2009e375f97e7499d
midorinoyama <fujiaqa@gmail.com>
2021-10-14 05:48:08 +0000

    Add postsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_11_045656) do
+ActiveRecord::Schema.define(version: 2021_10_14_051441) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "image_id"
+    t.string "title"
+    t.text "content"
+    t.string "mountain"
+    t.integer "prefecture", default: 0
+    t.date "filmed_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
