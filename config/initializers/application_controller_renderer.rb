4f7a069abd02116d6312ac87340cdf4a8bc2610e
midorinoyama <fujiaqa@gmail.com>
2021-10-18 09:52:23 +0000

    Add Refile.secret_keyエラー解決

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2a9890d47936174fadd714eee50767bf0b757d69cbe5110789d633599627fa22e862b6ece587b4eeda9d42d8d66ecf63b2691773f6b70bc73c599f8bc7831472'
