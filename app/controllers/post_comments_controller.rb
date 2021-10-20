37ad2a143d96d7d933ad695b50063190947c1f81
midorinoyama <fujiaqa@gmail.com>
2021-10-20 06:00:11 +0000

    Add　投稿詳細レイアウト

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,15 @@
 class PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     #コメントをする対象の投稿を見つける
     post_comment = PostComment.new(post_comment_params)
     #新規コメントを作成
     post_comment.user_id = current_user.id
     # (ログインしている)userに紐づいたコメントを取得
-    post_comment.post_id = post.id
+    post_comment.post_id = @post.id
     #投稿に紐づいたコメントを取得
     if post_comment.save
-      redirect_to post_path(post.id)
+      redirect_to post_path(@post.id)
     else
       render 'posts/show'
     end
@@ -24,7 +24,7 @@ class PostCommentsController < ApplicationController
 
   private
   def post_comment_params
-    params.require(:post_comment).permit(:comment)
+    params.require(:post_comment).permit(:comment, :parent_id)
   end
 
 end
