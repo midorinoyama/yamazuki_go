ebb60ee4dd5bbfaeee267542c5612ed98c73f557
midorinoyama <fujiaqa@gmail.com>
2021-10-26 08:50:36 +0000

    Add user/index 名前順に並び替え

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.page(params[:page]).per(16)
+    @users = User.page(params[:page]).per(16).order(nickname: :asc)# orderメソッドで名前の昇順
   end
 
   def show
