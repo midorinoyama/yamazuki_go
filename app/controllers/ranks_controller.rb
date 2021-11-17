49f7dba35489213c4a945843df31dc2a9324c2ad
midorinoyama <fujiaqa@gmail.com>
2021-11-17 03:19:52 +0000

    Add rank表示10件に変更

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,7 +1,7 @@
 class RanksController < ApplicationController
-  
+
   def rank
-    @post_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(6).pluck(:post_id))
-    # P探す/いいねテーブルでpost_idごとにグループ分け/数が多いpost_idを並べる/最大5件表示/post_idカラムのみ取り出す
+    @post_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(10).pluck(:post_id))
+    # P探す/いいねテーブルでpost_idごとにグループ分け/数が多いpost_idを並べる/最大10件表示/post_idカラムのみ取り出す
   end
 end
